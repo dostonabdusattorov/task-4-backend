d41ee43efb26fd13e34fea37e5625ac6f1a3d394
dostonabdusattorov <webdeveloperdoston@gmail.com>
2023-06-05 22:01:23 +0500

    sign in process started

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,31 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { SigninDto } from 'src/dtos/signin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authSer: AuthService) {}
 
   @Post('/signup')
-  async createUser(@Body() body: CreateUserDto) {
+  async signup(@Body() body: CreateUserDto) {
     try {
       return await this.authSer.signup(body.name, body.email, body.password);
     } catch (error) {
       throw new BadRequestException(error.message, { cause: error });
     }
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/signin')
+  signin(@Body() body: SigninDto) {
+    return this.authSer.signin(body.email, body.password);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/user.entity';
+import { Repository } from 'typeorm';
 
 const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-  constructor(private userSer: UserService) {}
+  constructor(
+    private userSer: UserService,
+    @InjectRepository(User) private repo: Repository<User>,
+  ) {}
 
   async signup(name: string, email: string, password: string) {
     const users = await this.userSer.find(email);
@@ -24,5 +30,29 @@ export class AuthService {
     const user = await this.userSer.create(name, email, result);
 
     return user;
+  }
+
+  async signin(email: string, password: string): Promise<any> {
+    const [user] = await this.userSer.find(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (hash.toString('hex') !== storedHash) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    const lastLoginTime = new Date().toString();
+    const signedinUser = this.repo.create({
+      ...user,
+      lastLoginTime,
+    });
+
+    return this.repo.save(signedinUser);
   }
 }
